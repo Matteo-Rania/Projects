5689ea73511a01fa505f3c8d1d3c7e13b6c654c4
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-03 17:49:08 +0100

    update mega-menu (completed), made responsive and update script

--- script/menu-btn.js
+++ script/menu-btn.js
@@ -1,13 +1,21 @@
 const menuBtn = document.querySelector('.mega-menu-button');
+const menuBox = document.querySelector('.mega-menu-window');
+const menuOut = document.querySelector('.mega-menu');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    //console.log("added open");
+    //to close menu when clicking outside
+    menuOut.addEventListener('click', (e) => {
+      //check if the click was in the mega-menu-window area
+      if (!menuBox.contains(e.target)) {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+      }
+    })
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    //console.log("removed open");
   }
 });
