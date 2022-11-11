c52943c46d377037d6e0d700893629b90b836c24
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-11 02:35:04 +0100

    minor fixes + update footer responsive layout [completed]  w/Silvio

--- script/footer-buttons.js
+++ script/footer-buttons.js
@@ -1,5 +1,6 @@
 const rollingstoneBtn = document.querySelector('.rollingstoneBtn');
 const legalBtn = document.querySelector('.legalBtn');
+const ourSitesBtn = document.querySelector('.our-sites-btn');
 
 rollingstoneBtn.addEventListener('click', () => {
     rollingstoneBtn.classList.toggle('active');
@@ -7,4 +8,8 @@ rollingstoneBtn.addEventListener('click', () => {
 
 legalBtn.addEventListener('click', () => {
     legalBtn.classList.toggle('active');
+});
+
+ourSitesBtn.addEventListener('click', () => {
+    ourSitesBtn.classList.toggle('active');
 });
